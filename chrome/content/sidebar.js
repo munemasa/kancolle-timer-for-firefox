628e1540dacccda94d8d8697caae22f18cb3d72d
amano <amano@miku39.jp>
2013-08-07 23:29:03 +0900

    ?????????????????????

--- chrome/content/sidebar.js
+++ chrome/content/sidebar.js
@@ -242,6 +242,27 @@ var KanColleTimerSidebar = {
 	}
     },
 
+    findWindow:function(){
+	let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+	let win = wm.getMostRecentWindow("KanColleTimerMainWindow");
+	return win;
+    },
+
+    /**
+     * 艦これタイマーを開く
+     */
+    open:function(){
+	let feature="chrome,resizable=yes";
+
+	let win = this.findWindow();
+	if(win){
+	    win.focus();
+	}else{
+	    let w = window.open("chrome://kancolletimer/content/mainwindow.xul","KanColleTimer",feature);
+	    w.focus();
+	}
+    },
+
     init: function(){
 	Application.console.log('KanColle Timer sidebar init.');
 	KanColleHttpRequestObserver.init();
