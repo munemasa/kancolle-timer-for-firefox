d6fa5e0d9778a88c993d9983ddaf9b47b46e8d1f
amano <amano@miku39.jp>
2013-11-24 00:02:55 +0900

    ????????????????? ???????????????????

--- chrome/content/shiplist.js
+++ chrome/content/shiplist.js
@@ -1,6 +1,69 @@
 Components.utils.import("resource://kancolletimermodules/httpobserve.jsm");
 
 var ShipList = {
+    allships: [],
+
+    clearListBox:function(){
+	let list = $('ship-list');
+
+	while( list.getRowCount() ){
+	    list.removeItemAt(0);
+	}
+    },
+
+    sort: function(type){
+	this.allships.sort( function(a,b){
+	    var tmpa = 0;
+	    var tmpb = 0;
+	    var order = -1;
+	    switch( type ){
+	    case 0: // 艦種
+		tmpa = a.stype;
+		tmpb = b.stype;
+		break;
+	    case 1: // レベル
+		tmpa = a.lv;
+		tmpb = b.lv;
+		break;
+	    case 2: // 状態
+		tmpa = a.cond;
+		tmpb = b.cond;
+		break;
+	    }
+	    return (tmpa - tmpb) * order;
+	});
+	this.setupListBox();
+    },
+
+    setupListBox:function(){
+	let list = $('ship-list');
+	
+	this.clearListBox();
+
+	let no = 1;
+	for( let k in this.allships ){
+	    let obj = this.allships[k];
+
+	    let elem = CreateElement('listitem');
+	    elem.appendChild( CreateListCell( no++ ) );
+	    elem.appendChild( CreateListCell( obj.type ) );
+	    elem.appendChild( CreateListCell( obj.name ) );
+	    elem.appendChild( CreateListCell( obj.lv ) );
+	    elem.appendChild( CreateListCell( obj.cond ) );
+
+	    if( obj.cond >=50 ){
+		elem.setAttribute('style','background-color: #ffffc0;');
+	    }else{
+		elem.setAttribute('style','background-color: white;');
+	    }
+
+	    for( let i in obj.equips ){
+		let name = obj.equips[i];
+		elem.appendChild( CreateListCell( name ) );
+	    }
+	    list.appendChild(elem);
+	}
+    },
 
     init: function(){
 	let now = GetCurrentTime();
@@ -10,27 +73,32 @@ var ShipList = {
 
 	document.title = "保有艦艇リスト "+ships.length+"隻 ("+GetDateString(now*1000)+")";
 
+	this.allships = new Array();
+
 	let list = $('ship-list');
 	let no = 1;
 	for( let k in ships ){
 	    let ship = ships[k];
-
-	    let elem = CreateElement('listitem');
 	    let data = FindShipData( ship.api_id );
-	    elem.appendChild( CreateListCell( no++ ) );
-	    elem.appendChild( CreateListCell( KanColleData.type_name[data.api_stype] ) );
-	    elem.appendChild( CreateListCell( data.api_name ) );
-	    elem.appendChild( CreateListCell( ship.api_lv) );
-	    elem.appendChild( CreateListCell( ship.api_cond) );
+	    let obj = new Object();
+
+	    obj.type = KanColleData.type_name[data.api_stype];
+	    obj.stype = data.api_stype;
+	    obj.name = data.api_name;
+	    obj.lv = ship.api_lv;
+	    obj.cond = ship.api_cond;
+
+	    obj.equips = new Array();
 
 	    for( let i in ship.api_slot ){
 		let slot_id = ship.api_slot[i];
 		if( slot_id==-1 ) continue;
 		let itemname = FindSlotItemNameById( slot_id );
-		elem.appendChild( CreateListCell( itemname) );
+		obj.equips.push( itemname );
 	    }
-	    list.appendChild(elem);
+	    this.allships.push( obj );
 	}
+	this.setupListBox();
 
 	// 艦隊編成
 	let fleets = KanColleRemainInfo.gDeckList;
