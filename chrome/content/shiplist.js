b69b3396e44b6b8c304c7e064c7c7eafc0ba06c0
amano <amano@miku39.jp>
2013-12-30 16:10:31 +0900

    ??????????

--- chrome/content/shiplist.js
+++ chrome/content/shiplist.js
@@ -211,4 +211,7 @@ var ShipList = {
 };
 
 
-window.addEventListener("load", function(e){ ShipList.init(); }, false);
+window.addEventListener("load", function(e){
+    ShipList.init();
+    WindowOnTop( window, $('window-stay-on-top').hasAttribute('checked') );
+}, false);
