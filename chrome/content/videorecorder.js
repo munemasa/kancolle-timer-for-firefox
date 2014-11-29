d9164ffbcc1575ccf46221e7286966d748690f89
amano <amano@miku39.jp>
2014-11-29 21:16:25 +0900

    ????FPS??????

--- chrome/content/videorecorder.js
+++ chrome/content/videorecorder.js
@@ -1,6 +1,4 @@
 var VideoRecorder = {
-    title: "艦これビデオレコーダー",
-
     fps: 15,
     timer_id: null,
 
@@ -9,11 +7,18 @@ var VideoRecorder = {
 
     seekbar: null,
 
+    setFPS: function( n ){
+	console.log( "Record " + n + " fps." );
+	this.fps = n;
+    },
+
     showFrame: function( frame_no ){
+	if( frame_no >= this.data.length ){
+	    frame_no = this.data.length - 1;
+	}
 	let frame = this.data[frame_no];
 	var ctx = $( 'video-playback' ).getContext( "2d" );
 	ctx.drawImage( frame, 0, 0, 800, 480 );
-
 	this.cnt = frame_no;
     },
 
@@ -53,7 +58,11 @@ var VideoRecorder = {
     tweet: function(){
 	let isjpeg = KanColleTimerConfig.getBool( "screenshot.jpeg" );
 	let url;
-	let canvas = this.data[ this.cnt ];
+	let n = this.cnt;
+	if( n >= this.data.length ){
+	    n = this.data.length - 1;
+	}
+	let canvas = this.data[ n ];
 	if( isjpeg ){
 	    url = canvas.toDataURL( "image/jpeg" );
 	}else{
