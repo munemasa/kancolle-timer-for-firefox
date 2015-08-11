68f6249e82eb131e3d7040c8a2cb26cbed5fa080
amano <amano@miku39.jp>
2015-08-12 00:09:54 +0900

    new???

--- chrome/content/sstweet.js
+++ chrome/content/sstweet.js
@@ -73,7 +73,7 @@ var SSTweet = {
 	ShowNotice( "スクリーンショットを送信しています...", true );
 	setTimeout( function(){
 	    let text = $('text').value;
-	    Twitter.updateStatusWithMedia(text, File(file.path));
+	    Twitter.updateStatusWithMedia(text, new File(file.path));
 	    //Twitter.updateStatus(text);
 	    setTimeout( function(){ $('send-button').disabled = false; }, 5000 );
 	}, 2000 );
